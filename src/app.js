0c858eafc3b1dce1fe91a4a1d5c3a1a1c71841ac
devilbd <vladymir.parchev@gmail.com>
2026-01-22 19:13:15 +0200

    ....

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ export class App {
     setupMainVariant() {
         this.mainMenu = new RadialContextMenu();        
         this.mainMenu.itemsSource = mainMenuItems;
-        this.mainMenu.selector = 'h1'; // Only on header for main variant
+        this.mainMenu.selector = '.content'; // Only on header for main variant
         this.mainMenu.onSelectItem = (item) => console.log("Main Menu Selected:", item.name);
         this.mainMenu.init();
     }
@@ -22,7 +22,7 @@ export class App {
     setupSvgVariant() {
         this.svgMenu = new RadialContextMenu();
         this.svgMenu.itemsSource = svgMenuItems;
-        this.svgMenu.selector = 'p'; // Only on paragraph for SVG variant
+        this.svgMenu.selector = '.content-svg'; // Only on paragraph for SVG variant
         this.svgMenu.onSelectItem = (item) => console.log("SVG Menu Selected:", item.name);
         this.svgMenu.onHover = (item) => console.log("SVG Menu Hover:", item.name);
         this.svgMenu.init();
